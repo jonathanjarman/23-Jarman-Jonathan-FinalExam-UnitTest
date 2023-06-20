ce9037fe804795de8dd27abdcedb955d79c86f46
Jonathan Jarman <jonathan.k.jarman@gmail.com>
2023-06-20 11:11:38 -0700

    Editing

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,18 +3,19 @@ import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class CalculatorTest implements StringCalculator {
+public class CalculatorTest extends StringCalculator {
     @Test
    void StringCalculatorTest() {
+        int result = add("5,6");
 
-        assertThrows(IllegalArgumentException.class, () -> add("5","-1"));
+        assertThrows(IllegalArgumentException.class, () -> add("5,6"));
 
     }
 
     @Test
     void StringCalculatorTest2() {
 
-        assertEquals(5, () -> add("5", "1222"));
+        assertEquals(5,add(String.valueOf(5), String.valueOf(2000)));
     }
 
 
